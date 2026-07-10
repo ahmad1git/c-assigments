ebfb8878107941fb6c67fbe219d3f374c5cf655d
ahmad1git <ahmadtamer127@gmail.com>
2026-07-10 20:27:19 +0300

    assigment-2.c

---
+++ assigment-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,res;
+    char operator;
+    printf("Please enter numebr a:");
+    scanf("%d",&a);
+    printf("Please enter number b:");
+    scanf("%d",&b);
+    printf("a+b = %d\n",a+b);
+    printf("a-b = %d\n",a-b);
+    printf("a&b = %d\n",a&b);
+    printf("a|b = %d\n",a|b);
+    printf("a^b = %d\n",a^b);
+    return 0;
+} 
